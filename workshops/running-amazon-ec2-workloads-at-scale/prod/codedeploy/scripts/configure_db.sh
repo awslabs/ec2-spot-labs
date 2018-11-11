358827883fd405b9d6f30d49c276768fc78f2d61
Chad Schmutzer (schmutze@) <schmutze@amazon.com>
2018-11-10 22:08:07 -0800

    workshop update

--- workshops/running-amazon-ec2-workloads-at-scale/prod/codedeploy/scripts/configure_db.sh
+++ workshops/running-amazon-ec2-workloads-at-scale/prod/codedeploy/scripts/configure_db.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST=runningamazonec2workloadsatscale.ckhifpaueqm7.us-east-1.rds.amazonaws.com/g' /var/www/koel/.env
-sed -i 's/APP_KEY=/APP_KEY=base64:1aswquzxBkVnbKbdeuyp5BuRp/dcrunZw1m9ZVYUj+c=/g' /var/www/koel/.env
