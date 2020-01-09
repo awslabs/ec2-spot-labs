0a856a8774a94e8027e18ec0ab10f89cb778edfc
schmutze <schmutze@amazon.com>
2020-01-08 21:33:29 -0800

    optimizer updates

--- aws-compute-optimizer/user-data.sh
+++ aws-compute-optimizer/user-data.sh
@@ -17,11 +17,21 @@ aws configure set default.region $REGION
 sed -i "s|region =|region = $REGION|g" /etc/awslogs/awscli.conf
 sed -i "s|cwlogs = cwlogs|cwlogs = $CLOUDWATCHLOGSGROUP|g" /etc/awslogs/awslogs.conf
 
-rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
-cp -av $WORKING_DIR/config.json /opt/aws/amazon-cloudwatch-agent/bin/
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
-
 systemctl enable awslogsd.service
 systemctl start awslogsd.service
+
+rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+cp -av $WORKING_DIR/config.json /opt/aws/amazon-cloudwatch-agent/bin/
+mkdir -p /usr/share/collectd/
+touch /usr/share/collectd/types.db
+systemctl enable amazon-cloudwatch-agent.service
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+
+
+
+
+
+
 
 
