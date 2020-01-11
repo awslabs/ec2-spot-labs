a1f34c029b6c163fed47376d7c8b194df304ec2c
schmutze <schmutze@amazon.com>
2020-01-10 16:04:23 -0800

    optimizer updates

---
+++ aws-compute-optimizer/lambda.py
@@ -0,0 +1,40 @@
+import boto3
+#def handler(event, context):
+  
+client = boto3.client('ssm')
+
+response = client.put_parameter(
+  Name='computeOptimizer1',
+  Value='c3.large',
+  Type='String',
+  Overwrite=True
+)
+print(response)
+
+response = client.put_parameter(
+  Name='computeOptimizer2',
+  Value='c4.large',
+  Type='String',
+  Overwrite=True
+)
+print(response)
+
+response = client.put_parameter(
+  Name='computeOptimizer3',
+  Value='c5.large',
+  Type='String',
+  Overwrite=True
+)
+print(response)
+
+client = boto3.client('cloudformation')
+response = client.update_stack(
+  StackName='opt4',
+  UsePreviousTemplate=True,
+  Capabilities=[
+      'CAPABILITY_IAM'
+  ]
+)
+print(response)
+  
+#return
