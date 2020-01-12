4658e54bba79a58954f0d0a72cd9d09edcf4276e
schmutze <schmutze@amazon.com>
2020-01-11 20:30:34 -0800

    optimizer updates

--- aws-compute-optimizer/lambda.py
+++ aws-compute-optimizer/lambda.py
@@ -1,40 +1,47 @@
 import boto3
-#def handler(event, context):
+
+def handler(event, context):
+
+  client = boto3.client('compute-optimizer')
+              
+  response = client.get_auto_scaling_group_recommendations(
+  )
+  print(response)
   
-client = boto3.client('ssm')
+  client = boto3.client('ssm')
 
-response = client.put_parameter(
-  Name='computeOptimizer1',
-  Value='c3.large',
-  Type='String',
-  Overwrite=True
-)
-print(response)
+  response = client.put_parameter(
+    Name='computeOptimizer1',
+    Value='c3.large',
+    Type='String',
+    Overwrite=True
+  )
+  print(response)
 
-response = client.put_parameter(
-  Name='computeOptimizer2',
-  Value='c4.large',
-  Type='String',
-  Overwrite=True
-)
-print(response)
+  response = client.put_parameter(
+    Name='computeOptimizer2',
+    Value='c4.large',
+    Type='String',
+    Overwrite=True
+  )
+  print(response)
 
-response = client.put_parameter(
-  Name='computeOptimizer3',
-  Value='c5.large',
-  Type='String',
-  Overwrite=True
-)
-print(response)
+  response = client.put_parameter(
+    Name='computeOptimizer3',
+    Value='c5.large',
+    Type='String',
+    Overwrite=True
+  )
+  print(response)
 
-client = boto3.client('cloudformation')
-response = client.update_stack(
-  StackName='opt4',
-  UsePreviousTemplate=True,
-  Capabilities=[
-      'CAPABILITY_IAM'
-  ]
-)
-print(response)
+  client = boto3.client('cloudformation')
+  #response = client.update_stack(
+  #  StackName='opt4',
+  #  UsePreviousTemplate=True,
+  #  Capabilities=[
+  #      'CAPABILITY_IAM'
+  #  ]
+  #)
+  print(response)
   
-#return
+return
